2fa31faa63b00cfbff2f7b48f8b4531d8bbd0950
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-02 19:30:05 +0530

    add Hello World

--- hello.cpp
+++ hello.cpp
@@ -1,18 +1,29 @@
 /* 
-#include<iostream> -> A file named iostream alredy exist. 
- iostream(Header file) => 'iostream' helps in input, ouput.
+#include<iostream>     -> A file named iostream alredy exist. 
+ iostream(Header file) => 'iostream' helps in input, ouput. The C++ language defines several headers, which contain information that is either necessary or useful to the program. 
+ For this program, the header <iostream> is needed.
 
-int main -> 
+using namespace std;   -> tells the compiler to use the std namespace. 
+
+int main -> int main() is the main function where program execution begins.
  int  => Integer. 'int' is return type.
  main => A function. 'main' is the entry point of the program.
  ()   => it represents funtion.
 
+
 {} -> {body}
 
-std::cout<<""; -> inside standard namespace a function named 'cout' is defined in imported header file(i.e. iostream).
- cout => it is a funtion
- std => Standard. 
- << => syntax to print the string inside "".
+cout << "Hello World"; -> causes the message "Hello World" to be displayed on the screen.
+ cout => it is a funtion.
+ <<   => syntax to print the string inside "".
+
+return 0; -> terminates main( )function and causes it to return the value 0 to the calling process.
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Steps to save the file, compile and run the program -
+=> Save the file as    : hello.cpp
+=> To compile the code : g++ hello.cpp (If there are no errors in the code, the command prompt will take you to the next line and would generate a.out executable file.)
+=> To run the program  : ./a.out
 */
 
 #include <iostream>
@@ -23,5 +34,3 @@ int main() {
    cout << "Hello World"; // prints Hello World
    return 0;
 }
-
-
