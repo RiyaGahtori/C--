224a316b42bfa4287efb1c002a51b117d3c359d4
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-03 06:26:42 +0530

    .

---
+++ C++_Basics/B2_Variables.cpp
@@ -0,0 +1,13 @@
+/*                                                      VARIABLES IN C++
+                                                       ------------------
+~ A Variable is a container to hold data. 
+
+~ Variable can be of various types. Primarily we have these variable type in C++ :
+1. int     : -1, 0, 1, 21
+2. Float   :  1.21, 1.111
+3. Char    :  'd', 'w', 'i'
+4. Doublle :  1.2111111111
+5. Boolean :  True or False 
+
+~ int sum = 21; means sum is an integer variable which holds value 21 in memory.
+*/    
