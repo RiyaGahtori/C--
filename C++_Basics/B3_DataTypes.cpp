210a1db95ba83899842b816862b45373e7d58b08
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-03 06:53:53 +0530

    .

---
+++ C++_Basics/B3_DataTypes.cpp
@@ -0,0 +1,14 @@
+/*                                                                 Data Types
+                                                                 ---------------
+~ Data types define the type of data a variable can hold, For e.g. an integer variable can hold integer data, a character type variable can hold character data, etc.
+
+~ Data types in C++ are categorised in three groups:
+a. Built-in      :  1. int     : -1, 0, 1, 21   [Integer value]
+                    2. Float   :  1.21, 1.111   [Decimal numbers of low precision]
+                    3. Char    :  'd', 'w', 'i' [Character values]
+                    4. Doublle :  1.2111111111  [Decimal numbers of high precision]
+                    5. Bool    :  True or False [Boolean value]
+
+b. User-defined  :
+c. Derived       :
+*/
