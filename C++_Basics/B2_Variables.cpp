90072cbd8b8fa3ee73eefefc7b1096e064f098c4
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-03 06:41:24 +0530

    .

--- C++_Basics/B2_Variables.cpp
+++ C++_Basics/B2_Variables.cpp
@@ -9,5 +9,18 @@
 4. Doublle :  1.2111111111
 5. Boolean :  True or False 
 
-~ int sum = 21; means sum is an integer variable which holds value 21 in memory.
+~ int sum  =  21; means sum is an integer variable which holds value 21 in memory.
+~ Syntax   :  Data_type  Variable_name = Value;   [int riri = 21;]
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                          Variable Scope 
+                                                         ----------------
+~ Scope of the variable is the region in code where the existence of variable is valid.
+
+~ Based on Scope, variable can be classified into two types:
+1. Local Variables   : Local variable are declared inside the braces of any function and can be accessed only from there.
+2. Gloabal Variables : Global variables are declared outside any function and can be accessed from anywhere.
 */    
+
+// Can global and local variable have same name in C++? : Yes.
+ 
