e1a8e41f086b5165a0054f02b27d019f7062225d
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-04 12:56:32 +0530

    .

---
+++ C++_Basics/B5_HeaderFiles.cpp
@@ -0,0 +1,17 @@
+/*
+There are two types of header files :
+1. System header file - It comes with the compiler
+2. User header file   - It is written by the programmer
+*/
+
+//1. System header file
+#include<iostream>  
+
+//2. User header file
+// #include"riri.h"  --> this will produce an error if 'riri.h' is not present in the current directory.
+using namespace std;
+
+int main()
+{
+    cout<<"Hiiii";
+}
