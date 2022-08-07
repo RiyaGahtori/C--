ab092613ce78507d9c742ad34c9b2554d89fca3a
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-07 16:56:05 +0530

    .

--- C++_Basics/B7_ReferenceVariable.cpp
+++ C++_Basics/B7_ReferenceVariable.cpp
@@ -3,17 +3,10 @@ using namespace std;
 
 int main()
 {   
-    //float, double and long double Literals
-    // float d = 34.4f;
-    // long double e = 34.4l;
-
-    // cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e<<endl;
-
-    // cout<<"The size of 34.4 is "<<sizeof(34.4)<<endl;
-    // cout<<"The size of 34.4f is "<<sizeof(34.4f)<<endl;
-    // cout<<"The size of 34.4F is "<<sizeof(34.4F)<<endl;
-    // cout<<"The size of 34.4l is "<<sizeof(34.4l)<<endl;
-    // cout<<"The size of 34.4L is "<<sizeof(34.4L)<<endl;
-
+    float x = 21;
+    float & y = x;
+    cout<<y<<endl;
+    cout<<x<<endl;
+    
     return 0;
 }     
