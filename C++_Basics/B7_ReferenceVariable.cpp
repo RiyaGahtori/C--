996a145be6be106cc6511f545f1f8214aaa20752
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-07 16:41:30 +0530

    .

--- C++_Basics/B7_ReferenceVariable.cpp
+++ C++_Basics/B7_ReferenceVariable.cpp
@@ -11,8 +11,9 @@ int main()
     // c=a+b;
     // cout<<"The sum of a and b is : "<<c;
 
-    float d = 34.4;
-    long double e = 34.4;
+    float d = 34.4f;
+    long double e = 34.4l;
+    
     cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e;
     return 0;
 }     
