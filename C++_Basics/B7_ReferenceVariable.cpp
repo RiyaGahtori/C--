a95f101fcd916cd6248223c929288f7d5b10b787
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-07 16:49:43 +0530

    sizeof()

--- C++_Basics/B7_ReferenceVariable.cpp
+++ C++_Basics/B7_ReferenceVariable.cpp
@@ -3,14 +3,6 @@ using namespace std;
 
 int main()
 {
-    // int a, b, c;
-    // cout<<"Enter the value of a  : ";
-    // cin>>a;
-    // cout<<"Enter the value of b  : ";
-    // cin>>b;
-    // c=a+b;
-    // cout<<"The sum of a and b is : "<<c;
-
     float d = 34.4f;
     long double e = 34.4l;
 
