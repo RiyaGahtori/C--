61e8f899cbb5188eb2b2b8928e62ff160c65854e
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-07 17:01:47 +0530

    .

---
+++ C++_Basics/B7_TypeCasting.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 21;
+    float b = 21.21;
+    
+    cout<<"The value of a is "<<(float)a<<endl;
+    cout<<"The value of a is "<<float(a)<<endl;
+
+    cout<<"The value of b is "<<(int)b<<endl;
+    cout<<"The value of b is "<<int(b)<<endl;
+
+    int c = int(b);
+
+    return 0;
+}
