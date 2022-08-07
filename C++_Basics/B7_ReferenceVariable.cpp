3b4de4b3303d7935908a2c2cf45679a5ef854583
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-07 16:52:26 +0530

    sizeof()

--- C++_Basics/B7_ReferenceVariable.cpp
+++ C++_Basics/B7_ReferenceVariable.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 
 int main()
-{
-    float d = 34.4f;
-    long double e = 34.4l;
+{   
+    //float, double and long double Literals
+    // float d = 34.4f;
+    // long double e = 34.4l;
 
-    cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e<<endl;
+    // cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e<<endl;
 
-    cout<<"The size of 34.4 is "<<sizeof(34.4)<<endl;
-    cout<<"The size of 34.4f is "<<sizeof(34.4f)<<endl;
-    cout<<"The size of 34.4F is "<<sizeof(34.4F)<<endl;
-    cout<<"The size of 34.4l is "<<sizeof(34.4l)<<endl;
-    cout<<"The size of 34.4L is "<<sizeof(34.4L)<<endl;
+    // cout<<"The size of 34.4 is "<<sizeof(34.4)<<endl;
+    // cout<<"The size of 34.4f is "<<sizeof(34.4f)<<endl;
+    // cout<<"The size of 34.4F is "<<sizeof(34.4F)<<endl;
+    // cout<<"The size of 34.4l is "<<sizeof(34.4l)<<endl;
+    // cout<<"The size of 34.4L is "<<sizeof(34.4L)<<endl;
 
     return 0;
 }     
