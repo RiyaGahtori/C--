a8b26087b449cd78234a358be320319da541b6e6
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-08 07:08:01 +0530

    .

--- C++_Basics/B8_Constants.cpp
+++ C++_Basics/B8_Constants.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int mian()
 {
+    //Variable 'a'
+    int a = 34;
+    cout<<"The value of a was : "<<a<<endl;
+    a = 21;
+    cout<<"The value of a is  : "<<a<<endl;
+
+    //Constant
+    const int b = 2;
+    cout<<"The value of b was : "<<b<<endl;
+/*  b = 6; --> error : assignment of read-only variable 'b'; 
+    cout<<"The value of b  is : "<<b<<endl;                   */
 
     return 0;
 }
