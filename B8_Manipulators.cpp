0419d182c7aac874a8efb2627b5b4223d40df48a
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-08 07:18:47 +0530

    setw()

---
+++ B8_Manipulators.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    int a = 2, b = 1234567890, c = 21;
+    cout<< "The value of a is : "<<a<<endl;
+    cout<< "The value of b is : "<<b<<endl;
+    cout<< "The value of c is : "<<c<<endl;
+//------------------------------------------
+    cout<< "The value of a is : "<<setw(4)<<a<<endl;
+    cout<< "The value of b is : "<<setw(4)<<b<<endl;
+    cout<< "The value of c is : "<<setw(4)<<c<<endl;
+
+    return 0;
+}
