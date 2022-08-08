262d97fd25832f1b64418382fef19044ff6530fa
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-08 07:10:03 +0530

    const

--- C++_Basics/B8_Constants.cpp
+++ C++_Basics/B8_Constants.cpp
@@ -8,7 +8,8 @@ int mian()
     cout<<"The value of a was : "<<a<<endl;
     a = 21;
     cout<<"The value of a is  : "<<a<<endl;
-
+/*-----------------------------------------------------------------------------*/
+   
     //Constant
     const int b = 2;
     cout<<"The value of b was : "<<b<<endl;
