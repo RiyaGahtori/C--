7040c27c01aa7777e09b1b4719aaed7caaebcd1c
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-08 08:07:07 +0530

    .

---
+++ C++_ControlStatements/C1_ControlStructures.cpp
@@ -0,0 +1,35 @@
+/*
+There are three types of control structures available in C and C++ :
+    1) Sequence structure (straight line paths)
+    2) Selection structure (one or many branches)
+    3)Loop structure (repetition of a set of activities)
+-------------------------------------------------
+
+1. if-else statement 
+  ------------------- 
+int i = 0;
+if(i<3)
+{
+    cout<<i;
+    i++;
+}  
+
+2. if-else ladder  
+  ----------------- 
+
+
+3. switch case
+  ---------------
+switch(expression)
+{
+    case 1:
+    {
+        action1;
+    }
+
+    default:
+    {
+        action 4;
+    }
+}
+*/
