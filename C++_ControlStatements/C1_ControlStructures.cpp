e81a27aac066fc63355c270a89edca475a2cd4e8
RiyaGahtori <riyagahtori21@gmail.com>
2022-08-14 10:26:30 +0530

    .

--- C++_ControlStatements/C1_ControlStructures.cpp
+++ C++_ControlStatements/C1_ControlStructures.cpp
@@ -32,4 +32,44 @@ switch(expression)
         action 4;
     }
 }
-*/
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Sequence  Structure
+    cout<<"Sequence  Structure"<<endl;
+    cout<<"--------------------"<<endl;
+    int A = 0;
+    cout<<"The value of A is : "<<A<<endl;
+    cout<<" "<<endl;
+    
+
+    //Selection Structure
+    cout<<"Selection Structure"<<endl;
+    cout<<"--------------------"<<endl;
+    int age;
+    cout<<"Tell me your age : ";
+    cin>>age;
+    cout<<endl;
+    if((age<18) && (age>0))
+    {
+        cout<<"Sorry! You can't come to my party"<<endl;
+    }
+    else if(age==18)
+    {
+        cout<<"Yaay! You will get a KID PASS for the party"<<endl;
+    }
+    else if(age<=0)
+    {
+        cout<<"Error!";
+    }
+    else
+    {
+        cout<<"You can come to the party"<<endl;
+    }
+    cout<<" "<<endl;
+    return 0;
+}
